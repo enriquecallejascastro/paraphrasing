3df43c960758c09ae683a70912c82cd59770256c
enriquecallejascastro <enrique.callejas412@gmail.com>
2022-03-25 08:58:22 +0000

    commit

--- paraphrasing_generation.py
+++ paraphrasing_generation.py
@@ -3,14 +3,31 @@
 import timeit
 import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+from sentence_splitter import SentenceSplitter, split_text_into_sentences
+
 
 model_name = 'tuner007/pegasus_paraphrase'
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
+# Paraphrasing a single sentence
 def get_response(input_text,num_return_sequences):
     batch = tokenizer.__call__([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
     translated = model.generate(**batch,max_length=60,num_beams=10, num_return_sequences=num_return_sequences, temperature=1.5)
     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
     return tgt_text
+
+
+# Paraphrasing a paragraph
+def from_list2paragraph(paragraph):
+    splitter = SentenceSplitter(language='en')
+    sentence_list = splitter.split(paragraph)
+    paraphrase = []
+    for i in sentence_list:
+        a = get_response(i,1)
+        paraphrase.append(a)
+    paraphrase2 = [' '.join(x) for x in paraphrase]
+    paraphrase3 = [' '.join(x for x in paraphrase2) ]
+    paraphrased_text = str(paraphrase3).strip('[]').strip("'")
+    return paraphrased_text

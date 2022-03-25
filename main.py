6e34150302b91442d37310d810663a0ee2ee5791
enriquecallejascastro <enrique.callejas412@gmail.com>
2022-03-25 09:24:28 +0000

    commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from paraphrasing_generation import get_response, from_list2paragraph
+from paraphrasing_generation import paraphrasing_paragraph, paraphrasing_sentence
 
 # Paraphrasing a single sentence
 text = 'We need to schedule new meeting times to include everyone.'
-get_response(text, 5)
+paraphrasing_sentence(text, 5)
 
 # Paraphrasing a paragraph
 context = "The highlight of this study is that the same light-weight model trained by keeping the objective of Paraphrase Generation can also be used for solving the Paraphrase Identification task. Hence, the proposed system islight-weight in terms of the model’s size along with the data used to train the model which facilitates the quick learning of the model without having to compromise with the results."
-from_list2paragraph(context)
+paraphrasing_paragraph(context)
--- paraphrasing_generation.py
+++ paraphrasing_generation.py
@@ -10,7 +10,14 @@ tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
 # Paraphrasing a single sentence
-def get_response(input_text,num_return_sequences):
+def paraphrasing_sentence(input_text,num_return_sequences):
+    '''
+    Returns n equivalent sentences of a string
+
+    Args:
+    - input_text: the sentence we want to paraphrase
+    - num_return_sequences: number of paraphrased sentence to generate
+    '''
     batch = tokenizer.__call__([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
     translated = model.generate(**batch,max_length=60,num_beams=10, num_return_sequences=num_return_sequences, temperature=1.5)
     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
@@ -18,14 +25,22 @@ def get_response(input_text,num_return_sequences):
 
 
 # Paraphrasing a paragraph
-def from_list2paragraph(paragraph):
+def paraphrasing_paragraph(input_text):
+    '''
+    Returns a paraphrased paragraph
+
+    Args:
+    - input_text: the paragraph we want to paraphrase
+    - num_return_sequences: number of paraphrased sentence to generate
+    '''
     splitter = SentenceSplitter(language='en')
-    sentence_list = splitter.split(paragraph)
+    sentence_list = splitter.split(input_text)
     paraphrase = []
     for i in sentence_list:
-        a = get_response(i,1)
+        a = paraphrasing_sentence(i,1)
         paraphrase.append(a)
     paraphrase2 = [' '.join(x) for x in paraphrase]
     paraphrase3 = [' '.join(x for x in paraphrase2) ]
     paraphrased_text = str(paraphrase3).strip('[]').strip("'")
     return paraphrased_text
+    
